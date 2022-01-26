899113d0c442c1d0d1ae6a654e1249a1af635cd1
Steve Rogers <steve.rog02@gmail.com>
2022-01-26 16:17:54 +0100

    init metamask

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,7 @@
         alt=""
       />
       <h1 class="mt-5">
-        All India Chess League 3.0 <br />
+        All India Chess League <span class="highlights">3.0</span><br />
         Claim your Badge
       </h1>
     </div>
@@ -22,7 +22,10 @@
               All you have to do is enter your email, enter the code you
               received via email and sign the transaction with Metamask.
             </h3>
-            <p>If you don't have Metamask follow this <a href="">guide</a></p>
+            <p>
+              <em>If you don't have Metamask follow this </em>
+              <a href="" target="_blank"><em>guide</em></a>
+            </p>
           </div>
         </div>
         <div v-if="step === 1">
@@ -92,6 +95,10 @@
 
 <script>
 // @ is an alias to /src
+import Web3 from "web3";
+import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+
 import Header from "@/components/Header.vue";
 
 export default {
@@ -102,12 +109,57 @@ export default {
   data() {
     return {
       step: 1,
+      account: "",
+      web3: "",
+      networks: {
+        ethereum: 1,
+        rinkeby: 4,
+        polygon: 137,
+        mumbai: 80001,
+        ganache: 5777,
+      },
+      abi: [],
+      network: "ethereum",
+      contract: "0x9Ec46a3dd227Fd5977C7F1997C043122a703FF2A",
+      explorerUrl: "https://etherscan.io/tx/",
     };
   },
   methods: {
     nextStep() {
       const app = this;
       app.step = app.step + 1;
+    },
+    async connect() {
+      const app = this;
+      const providerOptions = {
+        walletconnect: {
+          package: WalletConnectProvider,
+          options: {
+            infuraId: "insert_infura",
+          },
+        },
+      };
+      // Instantiating Web3Modal
+      const web3Modal = new Web3Modal({
+        cacheProvider: true,
+        providerOptions: providerOptions,
+      });
+      const provider = await web3Modal.connect();
+      app.web3 = await new Web3(provider);
+      // Checking if networkId matches
+      const netId = await app.web3.eth.net.getId();
+      if (netId !== 1) {
+        alert("Switch to Ethereum Network!");
+      } else {
+        const accounts = await app.web3.eth.getAccounts();
+        if (accounts.length > 0) {
+          app.balance = await app.web3.eth.getBalance(accounts[0]);
+          app.account = accounts[0];
+          app.balance = parseFloat(
+            app.web3.utils.fromWei(app.balance, "ether")
+          ).toFixed(10);
+        }
+      }
     },
   },
 };
